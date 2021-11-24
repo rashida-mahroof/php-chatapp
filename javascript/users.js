782adefabfd40a1f93e2d9499a9b339cc41c134b
Rashida Mahroof <rashidawebhash@gmail.com>
2021-11-24 21:07:23 +0530

    usersearch-users

--- javascript/users.js
+++ javascript/users.js
@@ -6,6 +6,31 @@ searchBtn.onclick = ()=>{
    searchBar.classList.toggle("active");
    searchBar.focus();
    searchBtn.classList.toggle("active");
+   searchBar.value ="";
+}
+
+searchBar.onkeyup = ()=>{
+   let searchTerm = searchBar.value; //getting user search value
+     //close users list when user search 
+   if(searchTerm != ""){
+      searchBar.classList.add("active");  //adding active class when user start searching
+   }else{
+      searchBar.classList.remove("active");     //
+   }
+      //lets start ajax
+     let xhr = new XMLHttpRequest(); //creating xml object
+     xhr.open("POST","php/search.php",true);
+     xhr.onload = ()=>{
+         if(xhr.readyState === XMLHttpRequest.DONE){
+             if(xhr.status === 200){
+                  let data = xhr.response;
+                  usersList.innerHTML = data;     //sending data to html
+                  
+             }
+         }
+     }
+     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     xhr.send("searchTerm=" + searchTerm);  //sending user search term to php file with ajax
 }
 
 setInterval(()=>{
@@ -16,8 +41,10 @@ setInterval(()=>{
        if(xhr.readyState === XMLHttpRequest.DONE){
            if(xhr.status === 200){
                 let data = xhr.response;
-                usersList.innerHTML = data;     //sending data to html
-                
+               
+                if(!searchBar.classList.contains("active")){      //if active not contains in search abr then add this
+                     usersList.innerHTML = data;
+                }     
            }
        }
    }
--- php/users.php
+++ php/users.php
@@ -6,19 +6,7 @@
     if(mysqli_num_rows($sql) == 1){
          $output .= "No users are available to chat";     //if no other users are notr available to chat,the only one is current logged in user
     }elseif(mysqli_num_rows($sql) > 1){
-        while($row = mysqli_fetch_assoc($sql)){
-            $output .= '<a href="">
-                        <div class="content">
-                            <img src="php/images/'.$row['image'].'" alt="">
-                            <div class="details">
-                                <span>' .$row['fname'] . " " .$row['lname']. '</span>
-                                <p>This is test message</p>
-                            </div>   
-                        </div>
-                        <div class="status-dot"><i class="fas fa-circle"></i></div>
-                        </a>
-                        ';
-        }
+        include "data.php";
     }
     echo $output; 
     ?>
